be1f112482316f6e9a2575520245ff6505111d3d
NabeelZaidi <37018555+NabeelZaidi@users.noreply.github.com>
2023-06-21 20:53:42 +0530

    first commit

---
+++ infra/index.ts
@@ -0,0 +1,58 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure from "@pulumi/azure-native";
+
+const configmyapp = new pulumi.Config('myapp');
+const config = new pulumi.Config('azure-native');
+const location = config.require("location")
+const resourceGroupName = configmyapp.require("resourceGroupName")
+const appServicePlanName = configmyapp.require("appServicePlanName")
+const webAppName = configmyapp.require("webAppName")
+
+// resource group
+const resourceGroup = new azure.resources.ResourceGroup(resourceGroupName, {
+    location: location, 
+    resourceGroupName: resourceGroupName
+});
+
+// app service plan
+const appServicePlan = new azure.web.AppServicePlan(appServicePlanName, {
+    name: appServicePlanName,
+    kind: "Linux",
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    sku: {
+        name: "S1",
+        tier: "Standard",
+    },
+},
+    {
+        dependsOn: resourceGroup
+    }
+);
+
+// webApp
+const webApp = new azure.web.WebApp(webAppName, {
+    name: webAppName,
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    serverFarmId: appServicePlan.id,
+    siteConfig: {
+        alwaysOn: true
+    }
+},
+    {
+        dependsOn: appServicePlan
+    }
+);
+
+// initial slot
+const slot = new azure.web.WebAppSlot("slot", {
+    name: webApp.name,
+    location: resourceGroup.location,
+    serverFarmId: appServicePlan.id,
+    resourceGroupName: resourceGroup.name,
+},
+    {
+        dependsOn: webApp
+    }
+);
